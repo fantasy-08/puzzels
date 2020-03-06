acf25c20561dc30565845a4ed1ce9bf6dee9b4eb
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-03-06 12:57:49 +0530

    DP
    dp started

---
+++ MAX_SUM_NO_ADJ.cpp
@@ -0,0 +1,33 @@
+//#pragma GCC optimize "trapv"//  WA to RE int!!
+#include <bits/stdc++.h>
+#define FOR(i,n) for(long long int i=0;i<n;i++)
+#define FORR(i,n) for(long long int i=n;i>=0;i--)
+#define pb push_back
+#define vlli vector<long long int>
+#define slli set<long long int>
+#define mlli map<long long int,long long int>
+#define lli long long int 
+#define test lli tt1234; cin>>tt1234;while(tt1234--)
+#define endl "\n"
+#define all(zz) zz.begin(),zz.end()
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define error(x) cerr << #x <<" is " << x << endl;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fint fast;test
+#define L(x)	((x)<<1)
+#define R(x)	(((x)<<1)+1)
+#define MOD 1000000007
+using namespace std;
+lli arr[100000],n;
+lli go(lli i){
+    if(i>=n) return 0;
+    if(arr[i]<0)
+        return go(i+1);
+    return max(arr[i]+go(i+2),go(i+1));
+}
+int main() {
+    cin>>n;
+    FOR(i,n) cin>>arr[i];
+    cout<<go(0)<<endl;
+    return 0;
+}
